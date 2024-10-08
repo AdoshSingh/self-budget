706ae6a8bf738d32aee472f4650fdfe5b500d6c7
AdoshSingh <adoshbhanoo@gmail.com>
2024-10-08 22:40:40 +0530

    minor change

--- components/AccountInfo.tsx
+++ components/AccountInfo.tsx
@@ -1,20 +1,48 @@
+"use client";
 import { useAppStore } from "@/store/store";
 import { Separator } from "./ui/separator";
 import { IndianRupee } from "lucide-react";
 import { AddTransaction } from "./AddTransaction";
 import { convertToCurrency } from "@/utils/formatNumber";
+import { useEffect, useState } from "react";
 
 const AccountInfo = () => {
-  const { account } = useAppStore();
+  const [totalBankBalance, setTotalBankBalance] = useState<number>();
+
+  const { account, funds, setFunds } = useAppStore();
   if (!account) {
     return <h2>Loading</h2>;
   }
+
+  useEffect(() => {
+    if (account && !funds) {
+      setFunds(account.id, null);
+    }
+
+    if (account && funds) {
+      const accountBalance =
+        account.primary_balance + account.secondary_balance;
+      let fundsBalance = 0;
+      funds.forEach((ele) => {
+        fundsBalance += ele.balance;
+      });
+      console.log(accountBalance + fundsBalance);
+      setTotalBankBalance(accountBalance + fundsBalance);
+    }
+  }, [account, funds]);
+
   return (
     <div className="p-6">
       <div className="flex justify-between items-center mb-4">
         <span className="text-2xl">Your Balances</span>
       </div>
       <div className="space-y-2">
+        <div className="flex justify-between font-bold">
+          <span>Total Bank Balance:</span>{" "}
+          <span className="flex items-center">
+            {convertToCurrency(totalBankBalance)}
+          </span>
+        </div>
         <div className="flex justify-between">
           <span>Primary Balance:</span>{" "}
           <span className="flex items-center">
